0ea4b0a0c4d6c0af1def6d23170d3dfa2983dcee
zhoujial <ZHou100200>
2019-04-17 10:52:36 +0800

    stream

--- src/main/java/com/example/HelloLambdaTest.java
+++ src/main/java/com/example/HelloLambdaTest.java
@@ -25,13 +25,21 @@ public class HelloLambdaTest {
         List<String> players = Arrays.asList(atp);
 
 // 以前的循环方式
+        players.stream().filter(string -> string.contains("R")).forEach(System.out::println);
         for (String player : players) {
-            System.out.print(player + "; ");
+            //System.out.print(player.replaceAll("a","") + "; ");
+//            if (player.contains("R")) {
+//                System.out.println(player);
+//            }
         }
-        System.out.print("-------------------------------------------------------");
+        System.out.print("-------------------------------------------------------1");
 // 使用 lambda 表达式以及函数操作(functional operation)
-        players.forEach((player) -> System.out.print(player + "; "));
-
+        players.forEach((player) -> {
+            if (player.contains("R")) {
+                System.out.print(player);
+            }
+        });
+        System.out.print("-------------------------------------------------------3");
 // 在 Java 8 中使用双冒号操作符(double colon operator)
         players.forEach(System.out::println);
     }
